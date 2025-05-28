f4d4ee84eef820e318253dff2ec1abe26374264b
Subba Reddy <subbareddy300@gmail.com>
2025-05-28 17:34:21 +0530

    index.html updated

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ExperienceComponent } from './pages/experience/experience.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ActivitiesComponent } from './pages/activities/activities.component';
+import { ResearchComponent } from './pages/research/research.component';
+import { PublicationsComponent } from './pages/publications/publications.component';
+import { NewsComponent } from './pages/news/news.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'experience', component: ExperienceComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'activities', component: ActivitiesComponent },
-  { path: '404', component: HomeComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: 'research', component: ResearchComponent },
+  { path: 'publications', component: PublicationsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: '**', redirectTo: '' }
 ];
